08c2dfc078c76ccc210044214859a21665777d57
ApothecaryMan <mohamedrezk.mar@gmail.com>
2025-11-05 19:52:25 +0200

    apply lazy loading for novel-list imgs

--- scripts/novel-switcher.js
+++ scripts/novel-switcher.js
@@ -195,7 +195,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       novelItem.dataset.novelId = novel.id;
 
       novelItem.innerHTML = `
-      <img class="novel-item-img" src="${novel.image}" alt="${novel.title}">  
+      <img class="novel-item-img lazy" data-src="${novel.image}" alt="${novel.title}" src="img/pic1.webp">  
         <div class="novel-item-details">
           <div class="novel-item-header">
             <p class="novel-title">${novel.title}</p>
@@ -218,6 +218,22 @@ document.addEventListener("DOMContentLoaded", async () => {
       console.log(
         `Appended novel: ${novel.title} with ${novel.chapterCount} chapters.`
       );
+    });
+
+    const lazyImages = novelListContainer.querySelectorAll(".lazy");
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          img.src = img.dataset.src;
+          img.classList.remove("lazy");
+          observer.unobserve(img);
+        }
+      });
+    });
+
+    lazyImages.forEach((img) => {
+      imageObserver.observe(img);
     });
   }
 
